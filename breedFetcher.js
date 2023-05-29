059ba61a33a27ceb70fed54358f72751bd1523ef
mattyu123 <matthewyu100@gmail.com>
2023-05-29 00:39:49 -0400

    test if the breed exists, if it doesn't then alert user

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,7 +5,15 @@ const url = `https://api.thecatapi.com/v1/breeds/search?q=${userBreed}`
 
 
 request(url, (error,response,body) => {
-  const data = JSON.parse(body)
-  console.log(data[0].description)
+  const data = JSON.parse(body);
 
-})
+  if (typeof data[0] === 'undefined') {
+    console.log("breed not found");
+  } 
+  
+  if (error) {
+    console.error(error);
+  }
+  
+  console.log(data[0].description);
+});
