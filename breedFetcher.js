1f18943376d10fade9da06caa13c40957eda6bfb
mattyu123 <matthewyu100@gmail.com>
2023-05-29 00:54:56 -0400

    run linter through code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,19 @@
 const request = require('request');
 const userBreed = process.argv[2];
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${userBreed}`
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${userBreed}`;
 
 
 request(url, (error,response,body) => {
   const data = JSON.parse(body);
 
-  if (typeof data[0] === 'undefined') {
-    console.log("breed not found");
-  } 
-  
   if (error) {
-    console.error(error);
-  }
+    console.log("THIS IS THE ERROR:", error);
   
-  console.log(data[0].description);
+  } else if (typeof data[0] === 'undefined') {
+    console.log("breed not found");
+
+  } else {
+    console.log(data[0].description);
+  }
 });
