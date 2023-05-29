11af6d85a3ff680e6f13f6dd45d15256febb65dd
mattyu123 <matthewyu100@gmail.com>
2023-05-29 00:17:33 -0400

    make dynamic by allowing users to specify breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,8 +1,11 @@
 const request = require('request');
+const userBreed = process.argv[2];
 
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=sib'
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${userBreed}`
 
-request(url, (response, body) => {
-  // console.log(response)
-  console.log(typeof body)
+
+request(url, (error,response,body) => {
+  const data = JSON.parse(body)
+  console.log(data[0].description)
+
 })
